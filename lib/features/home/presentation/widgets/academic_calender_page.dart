97fc0ac46d6e2c37d518ed25d54da1f4a9ae6e2d
Gaurav-Kushwaha-1225 <gaurav.family04@gmail.com>
2025-01-14 02:28:16 +0530

    Academic Calender Created

--- lib/features/home/presentation/widgets/academic_calender_page.dart
+++ lib/features/home/presentation/widgets/academic_calender_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class AcademicCalenderPage extends StatefulWidget {
   const AcademicCalenderPage({super.key});
@@ -16,6 +17,33 @@ class _AcademicCalenderPageState extends State<AcademicCalenderPage> {
         title: Text("Academic Calender",
             style: Theme.of(context).textTheme.bodyMedium),
         centerTitle: true,
+      ),
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Theme.of(context).colorScheme.onSurface,
+                width: 1.5,
+              ),
+            ),
+            clipBehavior: Clip.hardEdge,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: SfPdfViewer.asset(
+                'assets/pdf/2025_even.pdf',
+                enableTextSelection: false,
+                canShowPaginationDialog: false,
+                canShowTextSelectionMenu: false,
+                canShowHyperlinkDialog: false,
+                interactionMode: PdfInteractionMode.pan,
+                canShowSignaturePadDialog: false,
+                canShowScrollHead: false,
+                maxZoomLevel: 4,
+              ),
+            )),
       ),
     );
   }
