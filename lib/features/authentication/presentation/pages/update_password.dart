3aebadbb22d9cc9b03cf7e0d588187195ffb48a2
Gaurav-Kushwaha-1225 <gaurav.family04@gmail.com>
2025-01-15 02:34:37 +0530

    Updates update_password.dart

--- lib/features/authentication/presentation/pages/update_password.dart
+++ lib/features/authentication/presentation/pages/update_password.dart
@@ -1,13 +1,15 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:uhl_link/config/routes/routes_consts.dart';
+
+import '../../../../widgets/form_field_widget.dart';
 import '../../../../widgets/screen_width_button.dart';
 import '../bloc/user_bloc.dart';
-import '../../../../widgets/form_field_widget.dart';
 
 class UpdatePasswordPage extends StatefulWidget {
   final Map<String, dynamic> user;
+
   const UpdatePasswordPage({super.key, required this.user});
 
   @override
@@ -49,110 +51,119 @@ class _UpdatePasswordPageState extends State<UpdatePasswordPage> {
         }
       },
       child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).cardColor,
+          title: Text("Update Password",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(fontWeight: FontWeight.bold)),
+          centerTitle: true,
+        ),
         resizeToAvoidBottomInset: false,
-        body: SingleChildScrollView(
-          reverse: true,
+        body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
               children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                Image.asset("assets/images/logo.png"),
-                Text("Uhl Link", style: Theme.of(context).textTheme.titleLarge),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.12,
+                SingleChildScrollView(
+                  reverse: true,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.015),
+                        FormFieldWidget(
+                          focusNode: passwordFocusNode,
+                          fieldKey: passwordKey,
+                          controller: passwordTextEditingController,
+                          obscureText: true,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Please enter password.";
+                            }
+                            return null;
+                          },
+                          onChanged: (String? value) {},
+                          keyboardType: TextInputType.text,
+                          errorText: errorPasswordValue,
+                          prefixIcon: Icons.lock_outline_rounded,
+                          showSuffixIcon: true,
+                          hintText: "Password",
+                          textInputAction: TextInputAction.next,
+                        ),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.03),
+                        FormFieldWidget(
+                          focusNode: confirmPasswordFocusNode,
+                          fieldKey: confirmPasswordKey,
+                          controller: confirmPasswordTextEditingController,
+                          obscureText: true,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Please enter password.";
+                            } else if (passwordTextEditingController.text !=
+                                value) {
+                              return "Invalid Confirm Password.";
+                            }
+                            return null;
+                          },
+                          onChanged: (String? value) {},
+                          keyboardType: TextInputType.text,
+                          errorText: errorConfirmPasswordValue,
+                          prefixIcon: Icons.lock_outline_rounded,
+                          showSuffixIcon: true,
+                          hintText: "Confirm Password",
+                          textInputAction: TextInputAction.done,
+                        ),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02),
+                        Text.rich(TextSpan(children: [
+                          TextSpan(
+                              text: '* ',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelSmall
+                                  ?.copyWith(
+                                      color: Theme.of(context).primaryColor)),
+                          TextSpan(
+                              text:
+                                  "This will be your new password and updating the password can be done via some logged in device only.",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelSmall!
+                                  .copyWith(fontFamily: 'Montserrat_Regular'))
+                        ])),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.1),
+                      ]),
                 ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width - 40,
-                  child: Text("Update your Password",
-                      maxLines: 2,
-                      style: Theme.of(context).textTheme.titleMedium),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-                FormFieldWidget(
-                  focusNode: passwordFocusNode,
-                  fieldKey: passwordKey,
-                  controller: passwordTextEditingController,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter password.";
-                    }
-                    return null;
-                  },
-                  onChanged: (String? value) {},
-                  keyboardType: TextInputType.text,
-                  errorText: errorPasswordValue,
-                  prefixIcon: Icons.lock_outline_rounded,
-                  showSuffixIcon: true,
-                  hintText: "Password",
-                  textInputAction: TextInputAction.next,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-                FormFieldWidget(
-                  focusNode: confirmPasswordFocusNode,
-                  fieldKey: confirmPasswordKey,
-                  controller: confirmPasswordTextEditingController,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter password.";
-                    } else if (passwordTextEditingController.text != value) {
-                      return "Invalid Confirm Password.";
-                    }
-                    return null;
-                  },
-                  onChanged: (String? value) {},
-                  keyboardType: TextInputType.text,
-                  errorText: errorConfirmPasswordValue,
-                  prefixIcon: Icons.lock_outline_rounded,
-                  showSuffixIcon: true,
-                  hintText: "Confirm Password",
-                  textInputAction: TextInputAction.done,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.015),
-                Text.rich(TextSpan(children: [
-                  TextSpan(
-                      text: '* ',
-                      style: Theme.of(context)
-                          .textTheme
-                          .labelSmall
-                          ?.copyWith(color: Theme.of(context).primaryColor)),
-                  TextSpan(
-                      text:
-                          "This will be your new password and updating the password can be done via some logged in device only.",
-                      style: Theme.of(context)
-                          .textTheme
-                          .labelSmall!
-                          .copyWith(fontFamily: 'Montserrat_Regular'))
-                ])),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                ),
-                ScreenWidthButton(
-                  text: "Update Password",
-                  buttonFunc: () {
-                    bool? isPasswordValid =
-                        passwordKey.currentState?.validate();
-                    bool? isConfirmPasswordValid =
-                        confirmPasswordKey.currentState?.validate();
+                Positioned(
+                  bottom: 20,
+                  child: ScreenWidthButton(
+                    text: "Update Password",
+                    buttonFunc: () {
+                      bool? isPasswordValid =
+                          passwordKey.currentState?.validate();
+                      bool? isConfirmPasswordValid =
+                          confirmPasswordKey.currentState?.validate();
 
-                    if (isConfirmPasswordValid! && isPasswordValid!) {
-                      BlocProvider.of<AuthenticationBloc>(context).add(
-                          PasswordUpdateEvent(
-                              id: widget.user['id'],
-                              newPassword: passwordTextEditingController.text));
-                    }
-                  },
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                Padding(
-                    padding: EdgeInsets.only(
-                        bottom:
-                            MediaQuery.of(context).viewInsets.bottom * 0.5)),
-              ]),
+                      if (isConfirmPasswordValid! && isPasswordValid!) {
+                        BlocProvider.of<AuthenticationBloc>(context).add(
+                            PasswordUpdateEvent(
+                                id: widget.user['id'],
+                                newPassword:
+                                    passwordTextEditingController.text));
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
